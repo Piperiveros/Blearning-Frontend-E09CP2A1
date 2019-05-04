b7ace9207c1288c5d9cbf113a2df6fbe97daedf5
Piperiveros <felipe.riveros.f@gmail.com>
2019-05-04 09:28:31 -0300

    agrega modificaciones a index y js

--- test/app.js
+++ test/app.js
@@ -1,7 +1,24 @@
-$.ajax({
-	url: 'http://www.mindicador.cl/api',
-	method: 'GET'
-}).then(data){
-	console.log(data);
-};
+$(document).ready(function(){
 
+var url = 'http://www.mindicador.cl/api/';
+
+var indicadores = {
+Dólar: 'dolar',
+UTM: 'utm',
+UF: 'uf',
+EURO: 'euro'
+
+}
+
+	$('#select').on('change', function(){
+
+
+$.ajax({
+	url: url + indicadores[$(this).val()],
+	method: 'GET'
+}).done(function(data){
+	console.log(data);
+});
+
+})
+});
